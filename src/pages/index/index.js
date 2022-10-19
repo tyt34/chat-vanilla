76e43117875c6b6091bb8dd79125f4b9b2808d00
R N <novoto4in@mail.ru>
2022-10-19 17:58:29 +0300

    добавил несколько точек входа

---
+++ src/pages/index/index.js
@@ -0,0 +1,30 @@
+import './index.css'
+
+console.log(' i n d e x')
+
+import { io } from "https://cdn.socket.io/4.4.1/socket.io.esm.min.js"
+const socket = io("http://localhost:3000/")
+
+const but = document.querySelector('.block__link')
+
+const listenerButGiveName = function() {
+  console.log('1')
+  socket.emit("give a name");
+  console.log('2')
+  but.removeEventListener('click', listenerButGiveName)
+} 
+
+if (but) {
+  but.addEventListener('click', listenerButGiveName)
+}
+
+//but.removeEventListener()
+
+
+/*
+socket.emit("hello from client", 5, "6", { 7: Uint8Array.from([8]) });
+socket.on("hello from server", (...args) => {
+ console.log('0 --> ', ...args) 
+});
+*/
+//console.log(' --> ') 
--- webpack.config.js
+++ webpack.config.js
@@ -1,14 +1,17 @@
-const path = require('path');
-const HtmlWebpackPlugin = require('html-webpack-plugin'); 
-const { CleanWebpackPlugin } = require('clean-webpack-plugin'); 
+const path = require('path')
+const HtmlWebpackPlugin = require('html-webpack-plugin') 
+const { CleanWebpackPlugin } = require('clean-webpack-plugin') 
 
-const MiniCssExtractPlugin = require('mini-css-extract-plugin');
+const MiniCssExtractPlugin = require('mini-css-extract-plugin')
 
 module.exports = {
-  entry: { main: './src/pages/index.js' },
+  entry: { 
+    index: './src/pages/index/index.js', 
+    main: './src/pages/main/main.js' 
+  },
   output: {
     path: path.resolve(__dirname, 'dist'),
-    filename: 'main.js',
+    filename: '[name].js',
     publicPath: '',
     environment: { 
       module: true,
@@ -60,8 +63,181 @@ module.exports = {
         filename: 'index.html',
         template: './src/index.html',
         minify: false, 
+        chunks: ['index']
       }
     ),
+    new HtmlWebpackPlugin(
+      {
+        filename: 'main.html',
+        template: './src/main.html',
+        minify: false, 
+        chunks: ['main']
+      }
+    ),
+    new CleanWebpackPlugin(), 
+    new MiniCssExtractPlugin() 
+  ] 
+}
+
+
+/*
+const baseConf = {
+  mode: 'development',
+  devServer: {
+    static: {
+      directory: path.resolve(__dirname, 'app'),
+    },
+    compress: true,
+    port: 8080,
+    open: true
+  },
+  module: {
+  rules: [
+    {
+      test: /\.(png|svg|jpg|jpeg|gif|ico)$/,
+      type: 'asset/resource',
+      generator: {
+        filename: 'images/[name].[contenthash][ext]',
+      }
+    },
+    {
+      test: /\.(woff|woff2|eot|ttf|otf)$/i,
+      type: 'asset/resource',
+      generator: {
+        filename: 'fonts/[name].[contenthash][ext]',
+      }
+    },
+    {
+      test: /\.css$/,
+      use: [
+        MiniCssExtractPlugin.loader, {
+          loader: 'css-loader',
+          options: {
+            importLoaders: 1
+          }
+        },
+        'postcss-loader'
+      ]
+    }
+  ]
+  }
+}
+
+const index = {
+  entry: { main: './src/pages/index.js' },
+  output: {
+    path: path.resolve(__dirname, 'dist'),
+    filename: 'bundle1.js',
+    publicPath: '',
+    environment: { 
+      module: true,
+      dynamicImport: true, 
+    }
+  },
+  mode: 'development',
+  devServer: {
+    static: {
+      directory: path.resolve(__dirname, 'app'),
+    },
+    compress: true,
+    port: 8080,
+    open: true
+  },
+  module: {
+  rules: [
+    {
+      test: /\.(png|svg|jpg|jpeg|gif|ico)$/,
+      type: 'asset/resource',
+      generator: {
+        filename: 'images/[name].[contenthash][ext]',
+      }
+    },
+    {
+      test: /\.(woff|woff2|eot|ttf|otf)$/i,
+      type: 'asset/resource',
+      generator: {
+        filename: 'fonts/[name].[contenthash][ext]',
+      }
+    },
+    {
+      test: /\.css$/,
+      use: [
+        MiniCssExtractPlugin.loader, {
+          loader: 'css-loader',
+          options: {
+            importLoaders: 1
+          }
+        },
+        'postcss-loader'
+      ]
+    }
+  ]
+  },
+  plugins: [
+    new HtmlWebpackPlugin(
+      {
+        filename: 'index.html',
+        template: './src/index.html',
+        minify: false, 
+      }
+    ),
+    new CleanWebpackPlugin(), 
+    new MiniCssExtractPlugin() 
+  ] 
+}
+
+
+const main = {
+  entry: { main: './src/pages/main.js' },
+  output: {
+    path: path.resolve(__dirname, 'dist'),
+    filename: 'bundle2.js',
+    publicPath: '',
+    environment: { 
+      module: true,
+      dynamicImport: true, 
+    }
+  },
+  mode: 'development',
+  devServer: {
+    static: {
+      directory: path.resolve(__dirname, 'app'),
+    },
+    compress: true,
+    port: 8080,
+    open: true
+  },
+  module: {
+  rules: [
+    {
+      test: /\.(png|svg|jpg|jpeg|gif|ico)$/,
+      type: 'asset/resource',
+      generator: {
+        filename: 'images/[name].[contenthash][ext]',
+      }
+    },
+    {
+      test: /\.(woff|woff2|eot|ttf|otf)$/i,
+      type: 'asset/resource',
+      generator: {
+        filename: 'fonts/[name].[contenthash][ext]',
+      }
+    },
+    {
+      test: /\.css$/,
+      use: [
+        MiniCssExtractPlugin.loader, {
+          loader: 'css-loader',
+          options: {
+            importLoaders: 1
+          }
+        },
+        'postcss-loader'
+      ]
+    }
+  ]
+  },
+  plugins: [
     new HtmlWebpackPlugin(
       {
         filename: 'main.html',
@@ -72,4 +248,8 @@ module.exports = {
     new CleanWebpackPlugin(), 
     new MiniCssExtractPlugin() 
   ] 
-};
+}
+
+module.exports = [index, main]
+*/
+
