b9667361868aaeb883e2c36786036c9ff9e188ba
R N <novoto4in@mail.ru>
2022-10-19 15:17:38 +0300

    css и js вынесены из html благодаря вебпак, script вынесен в отдельный файл

--- webpack.config.js
+++ webpack.config.js
@@ -5,49 +5,53 @@ const { CleanWebpackPlugin } = require('clean-webpack-plugin');
 const MiniCssExtractPlugin = require('mini-css-extract-plugin');
 
 module.exports = {
-    entry: { main: './src/pages/index.js' },
-    output: {
+  entry: { main: './src/pages/index.js' },
+  output: {
     path: path.resolve(__dirname, 'dist'),
     filename: 'main.js',
-        publicPath: ''
+    publicPath: '',
+    environment: { 
+      module: true,
+      dynamicImport: true, 
+    }
   },
-    mode: 'development',
-      devServer: {
-        static: {
-          directory: path.resolve(__dirname, 'app'),
-        },
-        compress: true,
-        port: 8080,
-        open: true
+  mode: 'development',
+    devServer: {
+      static: {
+        directory: path.resolve(__dirname, 'app'),
       },
-    module: {
-      rules: [
-        {
-          test: /\.(png|svg|jpg|jpeg|gif|ico)$/,
-          type: 'asset/resource',
-          generator: {
-            filename: 'images/[name].[contenthash][ext]',
-          }
-        },
-        {
-          test: /\.(woff|woff2|eot|ttf|otf)$/i,
-          type: 'asset/resource',
-          generator: {
-            filename: 'fonts/[name].[contenthash][ext]',
-          }
-        },
-        {
-          test: /\.css$/,
-          use: [
-            MiniCssExtractPlugin.loader, {
-              loader: 'css-loader',
-              options: {
-                importLoaders: 1
-              }
-            },
-            'postcss-loader'
-          ]
+      compress: true,
+      port: 8080,
+      open: true
+    },
+  module: {
+    rules: [
+      {
+        test: /\.(png|svg|jpg|jpeg|gif|ico)$/,
+        type: 'asset/resource',
+        generator: {
+          filename: 'images/[name].[contenthash][ext]',
         }
+      },
+      {
+        test: /\.(woff|woff2|eot|ttf|otf)$/i,
+        type: 'asset/resource',
+        generator: {
+          filename: 'fonts/[name].[contenthash][ext]',
+        }
+      },
+      {
+        test: /\.css$/,
+        use: [
+          MiniCssExtractPlugin.loader, {
+            loader: 'css-loader',
+            options: {
+              importLoaders: 1
+            }
+          },
+          'postcss-loader'
+        ]
+      }
     ]
   },
   plugins: [
