e6db20ec1977ae3d5711dcd88036ffff608084f8
R N <novoto4in@mail.ru>
2022-10-20 20:26:02 +0300

    добавлены аватарки пользователей

--- src/pages/main/main.js
+++ src/pages/main/main.js
@@ -5,24 +5,24 @@ console.log(' M A I N ')
 import { io } from "https://cdn.socket.io/4.4.1/socket.io.esm.min.js"
 const socket = io("http://localhost:3000/")
 
-
-const string = document.querySelector('.main__user-name')
+const htmlNameMainUser = document.querySelector('.main__user-name')
+const htmlAvaMainUser = document.querySelector('.main__user-ava')
 let nameMainUser = ''
-//const firstUser = document.querySelector('.main__item')
-const tempUser = document.querySelector('.template-item')
-const listUsers = document.querySelector('.main__list')
-//let listItems = document.querySelectorAll('li')
+const htmlTempUser = document.querySelector('.template-item')
+const htmlListUsers = document.querySelector('.main__list')
 let usersList = []
 
+// https://random.imagecdn.app/100/100
 
 /**
  * Используется для присваивания имени текущему пользователю
  */
 socket.emit("give a name")
 
-socket.on("give a name", (userName) => {
-  string.textContent = 'Вы: ' + userName
-  nameMainUser = userName
+socket.on("give a name", (user) => {
+  htmlNameMainUser.textContent = 'Вы: ' + user.name
+  nameMainUser = user.name
+  htmlAvaMainUser.src = user.img
 })
 
 /**
@@ -31,9 +31,10 @@ socket.on("give a name", (userName) => {
 socket.on("now list users", (users) => {
   users.map( (user) => {
     if (user.name !== nameMainUser) {
-      let newUser = tempUser.content.cloneNode(true)
+      let newUser = htmlTempUser.content.cloneNode(true)
       newUser.querySelector('.main__item-text').textContent = user.name
-      listUsers.append(newUser)
+      newUser.querySelector('.main__user-ava').src = user.img
+      htmlListUsers.append(newUser)
     }
   })
   usersList = users
@@ -43,9 +44,10 @@ socket.on("now list users", (users) => {
  * Используется при подключение нового пользователя
  */
 socket.on('add new user', (newUserObj) => {
-  let newUser = tempUser.content.cloneNode(true)
+  let newUser = htmlTempUser.content.cloneNode(true)
   newUser.querySelector('.main__item-text').textContent = newUserObj.name
-  listUsers.append(newUser)
+  newUser.querySelector('.main__ava-mini').src = newUserObj.img
+  htmlListUsers.append(newUser)
   usersList.push(newUserObj)
 })
 
@@ -55,7 +57,6 @@ socket.on('add new user', (newUserObj) => {
 socket.on('remove user', (idUser) => {
   usersList.map( (user, i) => {
     if (user.id === idUser) {
-      let elForRem
       [...document.querySelectorAll('li')].map( (el) => {
         if (el.querySelector('.main__item-text').textContent === user.name) {
           el.remove()
@@ -67,7 +68,7 @@ socket.on('remove user', (idUser) => {
 })
 
 
-//string.textContent = 'la la la'
+//htmlNameMainUser.textContent = 'la la la'
 /*
 function displayName() {
   socket.emit("give a name")
@@ -86,16 +87,16 @@ function getName() {
   socket.emit("give a name")
   socket.on("give a name", (userName) => {
     console.log(' a: ', userName)
-    string.textContent = 'Вы: ' + userName
+    htmlNameMainUser.textContent = 'Вы: ' + userName
     //firstUser.querySelector('.main__item-text').textContent = userName
   })
   //console.log(' NAME: ')
   socket.on("now list users", (users) => {
     console.log(' all users: ', users)
     users.map( (user) => {
-      let newUser = tempUser.content.cloneNode(true)
+      let newUser = htmlTempUser.content.cloneNode(true)
       newUser.querySelector('.main__item-text').textContent = user.name
-      listUsers.append(newUser)
+      htmlListUsers.append(newUser)
     })
     usersList = users
   })
