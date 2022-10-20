3324228ecc89fe44f81179f1eef25fc4766e0bf3
R N <novoto4in@mail.ru>
2022-10-20 19:12:42 +0300

    реализовано корректное отображение пользователей которые сейчас онлайн

--- src/pages/main/main.js
+++ src/pages/main/main.js
@@ -7,33 +7,63 @@ const socket = io("http://localhost:3000/")
 
 
 const string = document.querySelector('.main__user-name')
-const firstUser = document.querySelector('.main__item')
+let nameMainUser = ''
+//const firstUser = document.querySelector('.main__item')
+const tempUser = document.querySelector('.template-item')
+const listUsers = document.querySelector('.main__list')
+//let listItems = document.querySelectorAll('li')
+let usersList = []
 
-function getName() {
-  socket.emit("give a name")
 
-  socket.on("give a name", (userName) => {
-    console.log(' a: ', userName)
-    string.textContent = 'Вы: ' + userName
-    firstUser.querySelector('.main__item-text').textContent = userName
-  })
+/**
+ * Используется для присваивания имени текущему пользователю
+ */
+socket.emit("give a name")
 
-  console.log(' NAME: ')
-
-  socket.on("now list users", (users) => {
-    console.log(' u: ', users)
-  })
-  
-}
-
-getName()
-
-socket.on('add new user', (newUserObj) => {
-  console.log(' add new user: ', newUserObj)
+socket.on("give a name", (userName) => {
+  string.textContent = 'Вы: ' + userName
+  nameMainUser = userName
 })
 
+/**
+ * Получение списка пользователей на момент подключения
+ */
+socket.on("now list users", (users) => {
+  users.map( (user) => {
+    if (user.name !== nameMainUser) {
+      let newUser = tempUser.content.cloneNode(true)
+      newUser.querySelector('.main__item-text').textContent = user.name
+      listUsers.append(newUser)
+    }
+  })
+  usersList = users
+})
+
+/**
+ * Используется при подключение нового пользователя
+ */
+socket.on('add new user', (newUserObj) => {
+  let newUser = tempUser.content.cloneNode(true)
+  newUser.querySelector('.main__item-text').textContent = newUserObj.name
+  listUsers.append(newUser)
+  usersList.push(newUserObj)
+})
+
+/**
+ * Используется при отключение пользователя
+ */
 socket.on('remove user', (idUser) => {
-  console.log(' remove user: ', idUser)
+  usersList.map( (user, i) => {
+    if (user.id === idUser) {
+      let elForRem
+      [...document.querySelectorAll('li')].map( (el) => {
+        if (el.querySelector('.main__item-text').textContent === user.name) {
+          el.remove()
+        }
+      })
+      usersList.splice(i, 1)
+    }
+  })
 })
 
 
@@ -49,4 +79,26 @@ function displayName() {
 }
 
 displayName()
+*/
+
+/*
+function getName() {
+  socket.emit("give a name")
+  socket.on("give a name", (userName) => {
+    console.log(' a: ', userName)
+    string.textContent = 'Вы: ' + userName
+    //firstUser.querySelector('.main__item-text').textContent = userName
+  })
+  //console.log(' NAME: ')
+  socket.on("now list users", (users) => {
+    console.log(' all users: ', users)
+    users.map( (user) => {
+      let newUser = tempUser.content.cloneNode(true)
+      newUser.querySelector('.main__item-text').textContent = user.name
+      listUsers.append(newUser)
+    })
+    usersList = users
+  })
+}
+getName()
 */
