07f609247e5b7c29cb866265f87496626461f3bf
R N <novoto4in@mail.ru>
2022-10-20 16:19:21 +0300

    добавил стили и получение данных о всех пользователей которые online

--- src/pages/main/main.js
+++ src/pages/main/main.js
@@ -6,21 +6,35 @@ import { io } from "https://cdn.socket.io/4.4.1/socket.io.esm.min.js"
 const socket = io("http://localhost:3000/")
 
 
-const string = document.querySelector('.block__welcome')
-
+const string = document.querySelector('.main__user-name')
+const firstUser = document.querySelector('.main__item')
 
 function getName() {
   socket.emit("give a name")
 
   socket.on("give a name", (userName) => {
     console.log(' a: ', userName)
-    string.textContent = userName
+    string.textContent = 'Вы: ' + userName
+    firstUser.querySelector('.main__item-text').textContent = userName
   })
 
   console.log(' NAME: ')
+
+  socket.on("now list users", (users) => {
+    console.log(' u: ', users)
+  })
+  
 }
 
 getName()
+
+socket.on('add new user', (newUserObj) => {
+  console.log(' add new user: ', newUserObj)
+})
+
+socket.on('remove user', (idUser) => {
+  console.log(' remove user: ', idUser)
+})
 
 
 //string.textContent = 'la la la'
